3296432528e01a88201839b72a6dbbf3a1860f56
Ashma Manandhar <ashmamdr11@gmail.com>
2020-02-08 14:50:17 -0600

    added js

---
+++ code/js/script.js
@@ -0,0 +1,28 @@
+function validateForm(){
+    var username = document.forms["loginForm"]["username"].value;
+    var password = document.forms["loginForm"]["password"].value;
+    if(username == "" || password == ""){
+        alert("Please enter all the required fields");
+        return false;
+    }
+    else if ((username == "Customer") && (password == "Customer123")){
+        alert("You are logged in as customer");
+        window.location.href = "mainPageEng.html";
+        return false;
+    }
+    else if ((username == "CustomerService") && (password == "Service123")){
+        alert("You are logged in as customer service");
+        window.location.href = "customerServiceEng.html";
+        return false;
+    }
+    else if ((username == "Admin") && (password == "Admin123")){
+        alert("You are logged in as admin");
+        window.location.href = "AdminPage.html";
+        return false;
+    }
+    else{
+        alert("Invalid password");
+        return false;
+    }
+    
+}
