80837c6aea69a23672543ab5b4bf60b83df393db
Ashma Manandhar <ashmamdr11@gmail.com>
2020-03-25 01:19:08 -0500

    only the first sql table works

--- code/database/db.js
+++ code/database/db.js
@@ -23,7 +23,7 @@ router.get('/', (req, res, next) => {
   res.render('');
 })
 
-//checks from CustomerService table
+//checks from Customer table
 con.query('SELECT username AS user, password AS pass FROM Customer', function (error, results, fields) {
   if (error) throw error;
   else{
@@ -36,13 +36,13 @@ con.query('SELECT username AS user, password AS pass FROM Customer', function (e
     var passwordInput = req.body.password;
     console.log('password input by user: ' + passwordInput);
     for(var b = 0; b < results.length; b++){
-      if(usernameInput == results[b].user && passwordInput == results[b].pass)
-        if (error) throw error;
-        else{
-          console.log('you entered the right credentials');
-          res.send("you've logged in successfully!");
-        }
-      }
+      if(usernameInput == results[b].user && passwordInput == results[b].pass){
+        console.log('you entered the right credentials');
+        res.send("you've logged in successfully!");
+      } 
+    }
+    res.send("error - annot find user in Customer db");
+    res.end();
     });
   }
 });
@@ -53,28 +53,29 @@ con.query('SELECT username AS user, password AS pass FROM Customer', function (e
 //checks from Admin table
 //for(var i = 0; i < tables.length; i++){
   //console.log('SELECT username AS user, password AS pass FROM ' + tables[i]);
-  con.query('SELECT username AS user, password AS pass FROM Admin', function (error, results, fields) {
-    if (error) throw error;
-    router.post('/login', (req, res, next) => {
-      console.log('checking Admin db');
-      //console.log('The correct username and password is: ', results[0].user, ', ' ,results[0].pass);
-      console.log('before checking');
-      var usernameInput = req.body.username;
-      console.log('user input: ' + usernameInput);
-      var passwordInput = req.body.password;
-      console.log('password input by user: ' + passwordInput);
-      for(var a = 0; a < results.length; a++){
-        if(usernameInput == results[a].user && passwordInput == results[a].pass)
-          if (error) throw error;
-            else{
-            console.log('you entered the right credentials');
-            res.send("you've logged in successfully!");
-            }
-        }
-    });
+con.query('SELECT username AS user, password AS pass FROM Admin', function (error, results, fields) {
+  if (error) throw error;
+  router.post('/login', (req, res, next) => {
+    console.log('checking Admin db');
+    //console.log('The correct username and password is: ', results[0].user, ', ' ,results[0].pass);
+    console.log('before checking');
+    var usernameInput = req.body.username;
+    console.log('user input: ' + usernameInput);
+    var passwordInput = req.body.password;
+    console.log('password input by user: ' + passwordInput);
+    for(var a = 0; a < results.length; a++){
+      if(usernameInput == results[a].user && passwordInput == results[a].pass){
+          console.log('you entered the right credentials');
+          //window.location.href = "AdminPage.html";
+          res.send("you've logged in as Admin!");
+          }
+      }
+      res.send('error - cannot find user in Admin db');
+      res.end();
   });
+});
 
-////checks from Customer table
+////checks from CustomerService table
 con.query('SELECT username AS user, password AS pass FROM CustomerService', function (error, results, fields) {
   if (error) throw error;
   router.post('/login', (req, res, next) => {
@@ -85,13 +86,13 @@ con.query('SELECT username AS user, password AS pass FROM CustomerService', func
     console.log('password input by user: ' + passwordInput);
     for(var i = 0; i < results.length; i++){
       //console.log('The correct username and password is: ', results[i].user, ', ' ,results[i].pass);
-      if(usernameInput == results[i].user && passwordInput == results[i].pass)
-        if (error) throw error;
-          else{
-            console.log('you entered the right credentials');
-            res.send("you've logged in successfully!");
+      if(usernameInput == results[i].user && passwordInput == results[i].pass){
+          console.log('you entered the right credentials');
+          res.send("you've logged in successfully!");
           }
       }
+      res.send("error - annot find user in CustomerService db");
+      res.end();
     });
 });
 
