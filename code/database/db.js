2dc8d71213f6169fdf84fac9e966fa61541e74b5
kajal7002 <kajal7002@gmail.com>
2020-03-25 09:28:57 -0500

    updated user and pass

--- code/database/db.js
+++ code/database/db.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const con = mysql.createPool({
   connectionLimit: 10,
   host: 'localhost',
-  user: 'ashma', //change this
-  password: 'password123', //change this
+  user: 'Kajal', //change this
+  password: 'kajal123', //change this
   database: 'smartkart' //make sure your sql database name is this or change it
 });
 
