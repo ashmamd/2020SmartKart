8d8779d056a2d80581eb2634d64970def2e1ae50
Darsh <dpatel137@hawk.iit.edu>
2020-04-21 20:57:25 -0500

    adding startingvagrant.sh

---
+++ install_scripts/startingvagrant.sh
@@ -0,0 +1,15 @@
+cd ../build
+vagrant box add ./ubuntu-db-18044-server-virtualbox-*.box --name db2020team14
+vagrant box list
+mkdir db2020team14
+cd db2020team14
+vagrant init db2020team14
+vagrant up
+
+cd ../
+vagrant box add ./ubuntu-ws-18044-server-virtualbox-*.box --name ws2020team14
+vagrant box list
+mkdir ws2020team14
+cd ws2020team14
+vagrant init ws2020team14
+vagrant up
