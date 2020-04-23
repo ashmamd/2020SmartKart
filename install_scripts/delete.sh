40cb700961260c943e9bacef673eebe65a54de28
kajal7002 <kajal7002@gmail.com>
2020-04-22 23:55:26 -0500

    added delete.sh file

---
+++ install_scripts/delete.sh
@@ -0,0 +1,14 @@
+cd ../build
+
+cd db2020team14
+vagrant global-status
+vagrant destroy default
+
+cd ../ws2020team14
+vagrant destroy default
+
+cd
+cd ~/2020-team14w/build
+rm -rf *
+rm ubuntu-db-18044-server-virtualbox-*.box
+rm ubuntu-ws-18044-server-virtualbox-*.box
